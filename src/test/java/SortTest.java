29c6fa72ead554f0ce063058136f906ab5c35c57
ahmed <ahmedatef07718@gmail.com>
2024-07-15 15:18:31 +0300

    sort

--- src/test/java/SortTest.java
+++ src/test/java/SortTest.java
@@ -8,6 +8,11 @@ public class SortTest extends TestBase{
     public void ValidSortingTC (){
 
       homePage  = new HomePage(driver);
+      sortPage = homePage.ClickSearchButton("web-development");
+      sortPage.clickonsortrelevant();
+      sortPage.clickonsortnewest();
+      sortPage.clickonsortrates();
+      sortPage.clickonsortreviews();
 
 
     }
